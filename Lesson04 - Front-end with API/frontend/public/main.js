ccd1c5642136b3dccbcd6b937c07f5d8d069a93b
Leonardo Camp <leofrederiche@gmail.com>
2022-06-20 21:52:52 -0300

    Lesson04 - Post method

--- Lesson04 - Front-end with API/frontend/public/main.js
+++ Lesson04 - Front-end with API/frontend/public/main.js
@@ -2,7 +2,6 @@
 const url = "http://localhost:3000/api"
 
 const getUsers = () => {
-    console.log("Consultando dados")
     fetch(url)
         .then( response => response.json())
         .then( data => apiResiltGetUsers.textContent = JSON.stringify(data) )
@@ -20,5 +19,27 @@ const getUser = (id) => {
         .catch( error => console.error(error))
 }
 
+const newUser = {
+    name: "Maria Eduarda",
+    avatar: "https://pps.whatsapp.net/v/t61.24694-24/225043864_138098855475195_3648358203373206951_n.jpg?ccb=11-4&oh=c6576f651b637cf0ddbadf9ff531e5a4&oe=62BF43C8",
+    city: "Pindamonhangaba"
+}
+
+const addUser = () => {
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+            "content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then( response => response.json() )
+        .then( data => {
+            apiNotification.textContent = data
+        })
+        .catch( error => console.log(error) )
+}
+
 getUsers()
-getUser(1)
+getUser(2)
+addUser()
