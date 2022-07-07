01f5cb3b4c71640f61e99da3384306e0310b1c20
Leonardo Camp <leofrederiche@gmail.com>
2022-07-06 21:24:42 -0300

    Lesson06 - State | Add student on list

--- Lesson06 - React/src/pages/Home/index.jsx
+++ Lesson06 - React/src/pages/Home/index.jsx
@@ -1,17 +1,43 @@
+import React, { useState } from "react"
 import "./styles.css"
 
 import { Card } from "../../components/Card"
 
 export function Home() {
+  const [name, setName] = useState('')
+  const [students, setStudents] = useState([])
+
+  function handleAddStudent() {
+    let time = new Date
+
+    const newStudent = {
+      name,
+      time: time.toLocaleTimeString()
+    }
+
+    setStudents(prevState => [...prevState, newStudent])
+  }
+
   return (
     <div className="form">
       <h1>Presence List</h1>
 
-      <input type="text" placeholder="Name" />
-      <button>Include</button>
+      <input 
+        type="text" 
+        placeholder="Name"
+        onChange={ e => setName(e.target.value) }
+      />
+      <button
+        onClick={handleAddStudent}
+      >
+        Include
+      </button>
 
-      <Card name="Leonardo" time="23:17:12"/>
-      <Card name="Maria Eduarda" time="22:15:11" />
+      {
+        students.map( (student, index) => (
+          <Card key={index} name={student.name} time={student.time} />
+        ))
+      }
     </div>
   )
 }
